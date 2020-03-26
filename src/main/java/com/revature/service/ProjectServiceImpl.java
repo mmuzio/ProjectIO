87684a8c6e921cde45c393e3f4d13f5ed570a1d0
mmuzio <mmuzio@hmc.edu>
2020-03-26 10:57:07 -0400

    minor changes again

--- src/main/java/com/revature/service/ProjectServiceImpl.java
+++ src/main/java/com/revature/service/ProjectServiceImpl.java
@@ -2,20 +2,36 @@ package com.revature.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.revature.domain.Project;
 import com.revature.domain.User;
+import com.revature.repository.ProjectRepository;
+import com.revature.repository.UserRepository;
 
 public class ProjectServiceImpl implements ProjectService {
 
+	private ProjectRepository projectRepository;
+	
+	private UserRepository userRepository;
+	
+	@Autowired
+	public void setProjectRepository(ProjectRepository projectRepository) {
+		this.projectRepository = projectRepository;
+	}
+	
+	@Autowired
+	public void setUserRepository(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
 	@Override
 	public Project getProjectById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return projectRepository.getOne(id);
 	}
 
 	@Override
 	public List<Project> getProjectsByUser(User user) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
